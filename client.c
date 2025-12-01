27999f3f36d47da7739db768fa608019bb326f86
AndrewJMart <amart89531@gmail.com>
2025-12-01 13:52:56 -0800

    First Try Client / Server Run

--- client.c
+++ client.c
@@ -1,16 +1,74 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 int main(int argc, char *argv[]) {
 
-    printf("Hello Client Code That is Nonexistent\n");
+    struct addrinfo hints, *res, *p;
+    int sockfd;
 
-    printf("Num Args: %d\n", argc);
-
-    for (int i = 0; i != argc; i++) {
-   
-        printf("Arg %d: %s\n", i, argv[i]);
-    
+    if (argc != 3) {
+        fprintf(stderr, "Usage: Client Hostname Port");
+        exit(1);
     }
+
+    // Set Up Hints For GetAddrInfo
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(argv[1], argv[2], &hints, &res) < 0) {
+       perror("getaddrinfo");
+       exit(1);
+    }
+
+    // Loop Through And Attempt To Connect To IPs
+    for (p = res; p != NULL; p = p->ai_next) {
+        // Process Current Address
+        int connect_status;
+
+        // 1 - Create Socket
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd < 0) {
+            perror("Socket");
+            continue;
+        }
+        
+        // 2 - Connect To Socket
+        connect_status = connect(sockfd, p->ai_addr, p->ai_addrlen);
+        if (connect_status < 0) {
+            perror("Connect");
+            continue;
+        }
+
+        // Once Connected, We Can Break As Connection Established
+        break;
+    }
+
+    // Check If Connection Formed
+    if (p == NULL) {
+        perror("Failed To Connect");
+        exit(1);
+    }
+    
+    // Receive From Server
+    int bytes_received;
+    char buf[1000];
+
+    bytes_received = recv(sockfd, buf, 999, 0);
+    if (bytes_received < 0) {
+       perror("recv");
+       exit(1);
+    } 
+
+    buf[bytes_recieved] = '\0';
+
+    printf("Recieved: %s\n", buf);
 
     return 0;
 
--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,44 +8,87 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define BACKLOG 10
+
 int main() {
 
-    int status;
+    int status, sockfd, bindnum;
     struct addrinfo hints;
     struct addrinfo *servinfo;
     char ipstr[INET6_ADDRSTRLEN];
+
+    // Get And Print Host Name For Client Use
+    char hostname[256];
+
+    if (gethostname(hostname, sizeof(hostname)) == -1) {
+        perror("gethostname");
+        exit(1);
+    }
+
+    printf("Host Name: %s\n", hostname);
+
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC; // IP Agnostic, Not Choosing Between V4/6
     hints.ai_socktype = SOCK_STREAM; // TCP Stream Sockets
     hints.ai_flags = AI_PASSIVE; // Fill In IP
+    
+    // Get Personal IP
+    getaddrinfo(NULL, "3490", &hints, &servinfo);
 
-    if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
-        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+    inet_ntop(servinfo->ai_family,
+          &((struct sockaddr_in*)servinfo->ai_addr)->sin_addr,
+          ipstr, sizeof ipstr);
+    printf("Server listening on IP: %s\n", ipstr);
+
+    // Make A Socket From Resulting IP
+
+    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+    if (sockfd < 0) {
+        perror("Socket Error\n");
         exit(1);
     }
 
-    void *addr;
-    void *ipver;
-    struct sockaddr_in *ipv4;
-    struct sockaddr_in6 *ipv6;
+    printf("Socket Number: %d", sockfd);
 
-    // Print Info 
-    if (servinfo->ai_family == AF_INET) {
-        ipv4 = (struct sockaddr_in *)servinfo->ai_addr;
-        addr = &(ipv4->sin_addr);
-        ipver = "IPv4";
-    } else {
-        ipv6 = (struct sockaddr_in6 *)servinfo->ai_addr;
-        addr = &(ipv6->sin6_addr);
-        ipver = "IPv6";
+    // Now Bind Socket To Port Passed Into GetAddrInfo
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+        perror("Server Error: Bind\n");
+        exit(1);
+    }
+    
+    // Now Have Server Listen On Said Port
+    if (listen(sockfd, BACKLOG) < 0) {
+        perror("Server Error: Listen\n");
+        exit(1);
     }
 
-    inet_ntop(servinfo->ai_family, addr, ipstr, sizeof ipstr);
-    printf("%s: %s\n", ipver, ipstr);
+
+    // Now Have Server Take Requests Infinitely
+    while (1) {
+        // Create New Stack Variables Per Acceptance
+        struct sockaddr_storage inc_addr;
+        socklen_t addr_size = sizeof inc_addr;
+        int acceptfd;
+
+        // Accept Incoming Requests
+        acceptfd = accept(sockfd, (struct sockaddr *)&inc_addr, &addr_size);
+
+        char *msg = "Does This Even Work?";
+        int len, bytes_sent;
+
+        len = strlen(msg);
+
+        // Send String Package
+        bytes_sent = send(acceptfd, msg, len, 0);
+
+        // Close Accept Socket?
+    }
+
+    // Close Socket FD ??
 
     freeaddrinfo(servinfo);
-
     return 0;
 
 }
